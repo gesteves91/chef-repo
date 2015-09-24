fb902fb421676fa27e21d8ce8cf51f1869633f55
gesteves91 <gesteves@avenuecode.com>
2015-09-24 19:48:00 -0300

    Uploading all my test cookbooks

--- cookbooks/wrapper/attributes/default.rb
+++ cookbooks/wrapper/attributes/default.rb
@@ -2,3 +2,7 @@ default['java']['accept_agreement'] = true
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['jdk_version'] = 8
 default['java']['install_flavor'] = 'oracle'
+
+default['java']['java_home'] = '/home/vagrant/'
+
+node.override['mongodb']['package_version'] = '3.0.6'
--- cookbooks/wrapper/recipes/default.rb
+++ cookbooks/wrapper/recipes/default.rb
@@ -11,5 +11,26 @@ include_recipe 'apt'
 include_recipe 'ark'
 include_recipe 'java'
 include_recipe "nodejs"
+include_recipe 'runit'
+include_recipe "mongodb::default"
 include_recipe "nodejs::npm"
-include_recipe 'wrapper::java'
+include_recipe "wrapper::java"
+include_recipe "git::default"
+
+git_client 'default' do
+    action :install
+end
+
+package 'git' do
+  action :install
+end
+
+
+#mongodb_instance "mongodb" do
+#    port node['application']['port']
+#end
+
+#mongodb_instance "my_instance" do
+#    port node['mongodb']['port'] + 100
+#      dbpath "/data/"
+#end
