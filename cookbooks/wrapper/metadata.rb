a65a145e2edd863c8caa0b7ac32fd22a2e4150bb
gesteves91 <gesteves@avenuecode.com>
2015-09-29 19:53:42 -0300

    Several changes in my repo

--- cookbooks/wrapper/metadata.rb
+++ cookbooks/wrapper/metadata.rb
@@ -1,6 +1,6 @@
 name             'wrapper'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Avenue Code LLC'
+maintainer_email 'gesteves@avenuecode.com'
 license          'All rights reserved'
 description      'Installs/Configures wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
